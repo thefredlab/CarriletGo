e39d87379fe575fccd4ffa424e3c2e5e1e7076c5
Frederik <53608074+N0chteil@users.noreply.github.com>
2026-03-14 22:25:43 +0100

    Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
+
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "@/styles/globals.css";
 
@@ -40,6 +42,7 @@ export default function RootLayout({
                 <body className={inter.className}>
                     {children}
                     <Analytics />
+                    <SpeedInsights />
                 </body>
             </html>
         </>
