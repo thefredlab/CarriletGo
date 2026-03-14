24bd72a9dc1c1fe47598f8991a887caa9a690c16
Frederik <53608074+N0chteil@users.noreply.github.com>
2026-03-14 22:09:22 +0100

    Update metadata and add Open Graph images

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,25 @@ import type { Metadata } from "next";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "CarriletGo - L'Escala Navigator",
-    description: "WIP"
+    title: "CarriletGO L'Escala Navigator – Train Maps & Schedules",
+    description:
+        "Explore L'Escala with CarriletGO! Your interactive guide for the tourist train. View live routes, all stops from Riells to Montgó, and up-to-date schedules.",
+    metadataBase: new URL("https://carrilet-go.com/"),
+
+    openGraph: {
+        type: "website",
+        images: [
+            {
+                url: "/opengraph-image.png",
+                width: 1200,
+                height: 628
+            }
+        ]
+    },
+
+    twitter: {
+        card: "summary_large_image"
+    }
 };
 
 export default function RootLayout({
