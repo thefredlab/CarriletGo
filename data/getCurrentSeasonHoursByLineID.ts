90cddbdca8850c0af5bd5ece7b91d5a11d7e5b64
7loud <53608074+7loud@users.noreply.github.com>
2026-03-23 14:34:22 +0100

    Improve logic to handle different seasons and dates

--- data/getCurrentSeasonHoursByLineID.ts
+++ data/getCurrentSeasonHoursByLineID.ts
@@ -6,20 +6,20 @@ import getMaundyThursdayDate from "@/utils/getMaundyThursdayDate";
  *
  * @param {string} lineID - The unique identifier for the line. Examples include "yellow" or "blue".
  *
- * @return {Array<{startHour: number, lastHour: number}>} An array of objects representing time ranges for current season hours.
+ * @return {Array<{startHour: number, lastHour: number}>} Returns an array with one object if there's no break. If there's a break, it returns an array with two objects.
  * Each object specifies the startHour and lastHour. An empty array is returned if no hours are applicable.
  */
-export default function getCurrentSeasonHoursByLineID(lineID: string) {
+export default function getCurrentSeasonHoursByLineID(lineID: string): Array<{ startHour: number; lastHour: number; }> {
     const currentDate = new Date();
 
     // General data checkpoints
     // June 11th to July 10th
-    const isJunetoJuly =
+    const isJuneToJuly =
         (currentDate.getMonth() === 5 && currentDate.getDate() >= 11) ||
         (currentDate.getMonth() === 6 && currentDate.getDate() < 11);
 
     // July 11th to August 31st
-    const isJulytoAugust =
+    const isJulyToAugust =
         (currentDate.getMonth() === 6 && currentDate.getDate() >= 11) ||
         (currentDate.getMonth() === 7 && currentDate.getDate() < 31);
 
@@ -27,16 +27,16 @@ export default function getCurrentSeasonHoursByLineID(lineID: string) {
         // Specific date checkpoints
         // Easter season
         const easterSeasonStart = getMaundyThursdayDate(),
-            easterSeasonEnd = new Date(currentDate.getFullYear(), 6, 10),
+            easterSeasonEnd = new Date(easterSeasonStart.getDate() + 5),
             isEasterSeason =
                 currentDate >= easterSeasonStart &&
                 currentDate <= easterSeasonEnd;
 
         // Weekends from April to June
         const isWeekendAprilToJune =
-            (currentDate.getDay() === 0 || currentDate.getDay() === 5) &&
-            currentDate.getMonth() >= 3 &&
-            currentDate.getMonth() <= 5;
+            (currentDate.getDay() === 0 || currentDate.getDay() === 5 || currentDate.getDay() === 6) &&
+            (currentDate.getMonth() >= 3 ||
+                (currentDate.getMonth() <= 5 && currentDate.getDate() < 11));
 
         // September 1st to September 15th
         const isSeptember =
@@ -58,7 +58,7 @@ export default function getCurrentSeasonHoursByLineID(lineID: string) {
                     }
                 ];
 
-            case isJunetoJuly:
+            case isJuneToJuly:
             case isSeptember:
                 return [
                     {
@@ -71,7 +71,7 @@ export default function getCurrentSeasonHoursByLineID(lineID: string) {
                     }
                 ];
 
-            case isJulytoAugust:
+            case isJulyToAugust:
                 return [
                     {
                         startHour: 8,
@@ -88,7 +88,7 @@ export default function getCurrentSeasonHoursByLineID(lineID: string) {
         const isSeptember = currentDate.getMonth() === 8;
 
         switch (true) {
-            case isJunetoJuly:
+            case isJuneToJuly:
             case isSeptember:
                 return [
                     {
@@ -101,7 +101,7 @@ export default function getCurrentSeasonHoursByLineID(lineID: string) {
                     }
                 ];
 
-            case isJulytoAugust:
+            case isJulyToAugust:
                 return [
                     {
                         startHour: 9,
