50ee557a40bf379e3222cd01fa135d40f0c53939
7loud <53608074+7loud@users.noreply.github.com>
2026-03-23 15:18:04 +0100

    Refactor stop info pop-up content to use StopInfoPopUp component. Move PopUp to separate dir for better organization

---
+++ components/popup/StopInfoPopUp.tsx
@@ -0,0 +1,86 @@
+import getDepartureTimesByStopID from "@/data/getDepartureTimesByStopID";
+import getStopByID from "@/data/getStopByID";
+
+import Image from "next/image";
+
+import styles from "./StopInfoPopUp.module.css";
+
+import type { Dispatch, SetStateAction } from "react";
+
+export default function StopInfoPopUp({ stopID, setStart, setDestination }: {
+    stopID: number;
+    setStart?: Dispatch<SetStateAction<any>>,
+    setDestination?: Dispatch<SetStateAction<any>>
+}) {
+    const stop = getStopByID(stopID),
+        departures = getDepartureTimesByStopID(stopID);
+
+    return <>
+        <header className={styles.heading}>
+            <Image
+                className={styles.stopIcon}
+                src={`/stops/${stopID}.png`}
+                alt={stopID.toString()}
+                width={100}
+                height={100}
+            />
+            <h2 className={styles.title}>
+                {stop.name ?? stop.customName}
+            </h2>
+        </header>
+
+        <h1 className={styles.timetableTitle}>
+            Next Departures
+        </h1>
+
+        <div className={styles.timetable}>
+            {departures && departures.formatted.length > 0 ? (
+                departures.formatted.map((time, i) => (
+                    <span
+                        key={time + i}
+                        className={styles.departure}
+                    >
+                                        {time}
+                                    </span>
+                ))
+            ) : (
+                <p className={styles.noTimetable}>
+                    Sorry, we couldn&apos;t find any departures
+                    for this stop. Please try again later.
+                </p>
+            )}
+        </div>
+
+        {setStart && setDestination && (
+            <div className={styles.buttons}>
+                <div
+                    className={styles.button}
+                    onClick={() => {
+                        setStart({
+                            name: stop.customName || stop.name,
+                            address: {},
+                            lat: stop.location.lat,
+                            lng: stop.location.lng
+                        });
+                    }}
+                >
+                    Set as Start
+                </div>
+
+                <div
+                    className={styles.button}
+                    onClick={() =>
+                        setDestination({
+                            name: stop.customName || stop.name,
+                            address: {},
+                            lat: stop.location.lat,
+                            lng: stop.location.lng
+                        })
+                    }
+                >
+                    Set as Destination
+                </div>
+            </div>
+        )}
+    </>;
+}
